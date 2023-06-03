eda6a78553026d0701e59ccdccc30f7ce5cb7d97
alirezakh74 <alirezakhodabande74@gmail.com>
2023-06-03 14:52:36 +0330

    define DB consts

--- app/core/config.php
+++ app/core/config.php
@@ -3,6 +3,11 @@
 if($_SERVER['SERVER_NAME'] == 'localhost')
 {
     define('ROOT', 'http://localhost/mvc/public/');
+    define("DB_USERNAME", "root");
+    define("DB_PASS", "");
+    define("DB_DRIVER_NAME", "mysql");
+    define("DB_HOST", "localhost");
+    define("DB_NAME", "my_mvc_db");
 }
 else
 {
