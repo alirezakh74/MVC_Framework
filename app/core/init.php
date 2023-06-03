20ea7078add00ed21438a4cca732a077af3c01e9
alirezakh74 <alirezakhodabande74@gmail.com>
2023-06-03 14:54:27 +0330

    auto loader for models

--- app/core/init.php
+++ app/core/init.php
@@ -1,6 +1,12 @@
 <?php
 
+spl_autoload_register(function($class_name){
+    $filename = "../app/models/".ucfirst($class_name).".php";
+    require_once($filename);
+});
+
 require_once("config.php");
+require_once("functions.php");
 require_once("Database.php");
 require_once("Model.php");
 require_once("Controller.php");
