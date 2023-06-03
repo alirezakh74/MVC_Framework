37906384277abbd8cf6db6365a09676188df2047
alirezakh74 <alirezakhodabande74@gmail.com>
2023-06-03 14:52:09 +0330

    test User model

--- app/controllers/Home.php
+++ app/controllers/Home.php
@@ -4,6 +4,8 @@ class Home extends Controller
 {
     public function index()
     {
+        $user = new User;
+        $user->insert(['first_name' => 'Ali', 'last_name' => 'Rezaie', 'age' => 30]);
         echo "this is index of home page<br>";
         $this->view('home');
     }
